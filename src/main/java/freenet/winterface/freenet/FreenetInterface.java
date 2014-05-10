459a2852b1b1a9882b47bf12d429fc406b0fc13b
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2014-05-10 15:48:34 +0200

    Finish incomplete sentence.

--- src/main/java/freenet/winterface/freenet/FreenetInterface.java
+++ src/main/java/freenet/winterface/freenet/FreenetInterface.java
@@ -1,7 +1,8 @@
 package freenet.winterface.freenet;
 
 /**
- * Defines the interface between Winterface and the Freenet node. All methods in this interface should
+ * Defines the interface between Winterface and the Freenet node. All methods
+ * in this interface should return as fast as possible.
  *
  * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
  */
